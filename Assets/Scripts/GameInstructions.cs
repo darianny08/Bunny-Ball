068fe6225f73b5dd7e03ca7a9779e4261363a6ae
darianny08 <146846855+darianny08@users.noreply.github.com>
2024-05-14 09:18:23 -0400

    Initial commit

---
+++ Assets/Scripts/GameInstructions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstructions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       Debug.Log ("Use W, A, S, D to control the character. Go through the maze and collect the key!"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
